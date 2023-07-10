45bb2a204af9fcf1084f8d9e92ad6a68430d2be5
yellowbee686 <yellowbee686@gmail.com>
2023-07-10 19:56:55 +0800

    debug

--- if_pipeline_demo.py
+++ if_pipeline_demo.py
@@ -5,7 +5,7 @@ import torch
 pipe = IFPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16", torch_dtype=torch.float16, local_files_only=True)
 pipe.enable_model_cpu_offload()
 
-prompt = 'A Chinese young swordman named LiBai, weared a rough belt, The blade of Wugou sword was as bright as frost and snow. The silver saddle lighted the white horse, Rustling like a meteor when running'
+prompt = 'A Chinese young , handsome swordman named Li Bai, The blade of Wugou sword was as bright as frost and snow. The silver saddle lighted the white horse, Rustling like a meteor when running'
 prompt_embeds, negative_embeds = pipe.encode_prompt(prompt)
 
 image = pipe(prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_embeds, output_type="pt").images
@@ -25,6 +25,7 @@ pil_image[0].save("./demo/if_stage_2.png")
 
 # numpy_img = pt_to_numpy(image)
 
+# if model safety_checker need numpy input, but sd-upscaler's input is pil_image to tensor
 safety_modules = {
     "feature_extractor": pipe.feature_extractor,
     # "safety_checker": super_res_1_pipe.safety_checker,
@@ -35,6 +36,7 @@ super_res_2_pipe = DiffusionPipeline.from_pretrained(
 )
 super_res_2_pipe.enable_model_cpu_offload()
 
+# pass image will cause warning, use pil_image instead
 image = super_res_2_pipe(
     prompt=prompt,
     image=pil_image,
