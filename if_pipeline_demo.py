a29a89a7e0ad9af330c9f3eb28ead9102a22e442
yellowbee686 <yellowbee686@gmail.com>
2023-07-10 20:48:52 +0800

    no watermaker

--- if_pipeline_demo.py
+++ if_pipeline_demo.py
@@ -5,9 +5,8 @@ import torch
 pipe = IFPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16", torch_dtype=torch.float16, local_files_only=True)
 pipe.enable_model_cpu_offload()
 
-# prompt = 'A Chinese young, handsome swordman named Li Bai, The blade of Wugou sword was as bright as frost and snow. The silver saddle lighted the white horse, Rustling like a meteor when running'
-# prompt = "A Chinese young, handsome swordman, wears his sword gleams very brilliantly, shows a heroic spirit. he ride a gallant white steed. When they gallop, their combined force is like a swift meteor, streaking across the vast expanse, a poignant picture of might and elegance."
-prompt = "With animation style, An east-asian young, handsome swordman, wears his sword gleams very brilliantly. he ride a gallant white steed. His speed is like shadow or swift meteor."
+# prompt = "With animation style, An east-asian young, handsome swordman, wears his sword gleams very brilliantly. he ride a gallant white steed. His speed is like shadow or swift meteor."
+prompt = "1 girl, beautiful Korean girl, (Cute Loose Bob hair), (wearing a cropped hoodie, capri sweatpants:1. 5), (hands in pockets:1. 5), (small breasts:1. 3), (eyelashes:1. 2), beautiful detailed eyes, symmetrical eyes, (detailed face), immersive background, volumetric haze, global illumination, soft lighting, (flowing hair), (bright smile), natural lighting, (realistic:1. 5), (lifelike:1. 4), (4k, digital art, masterpiece), High detail digital painting, realistic, (top quality), (soft shadows), (best character art), ultra high resolution, highly detailed digital artwork, physically-based rendering, realism with an artistic touch, vibrant colors, f2. 2 lens, soft palette, natural beauty."
 prompt_embeds, negative_embeds = pipe.encode_prompt(prompt)
 
 image = pipe(prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_embeds, output_type="pt").images
