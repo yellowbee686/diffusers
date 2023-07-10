4d0cf55411967847b155ed377f9add35107d0b67
yellowbee686 <yellowbee686@gmail.com>
2023-07-10 17:45:41 +0800

    stable-diffusion-x4-upscaler callable fix

--- if_pipeline_demo.py
+++ if_pipeline_demo.py
@@ -1,11 +1,11 @@
 from diffusers import IFPipeline, IFSuperResolutionPipeline, DiffusionPipeline
-from diffusers.utils import pt_to_pil
+from diffusers.utils import pt_to_pil, pt_to_numpy
 import torch
 
 pipe = IFPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16", torch_dtype=torch.float16, local_files_only=True)
 pipe.enable_model_cpu_offload()
 
-prompt = 'extreme closeup candid shot of a Chinese young woman, wavy brown hair, showing every detail on her face, realistic skin, natural beauty, EOS 5D, 200mm lens, --ar 4:3 --v 5.2'
+prompt = 'A Chinese young swordman named LiBai, weared a rough belt, The blade of Wugou sword was as bright as frost and snow. The silver saddle lighted the white horse, Rustling like a meteor when running'
 prompt_embeds, negative_embeds = pipe.encode_prompt(prompt)
 
 image = pipe(prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_embeds, output_type="pt").images
@@ -23,6 +23,8 @@ image = super_res_1_pipe(image=image, prompt_embeds=prompt_embeds, negative_prom
 pil_image = pt_to_pil(image)
 pil_image[0].save("./demo/if_stage_2.png")
 
+numpy_img = pt_to_numpy(image)
+
 safety_modules = {
     "feature_extractor": pipe.feature_extractor,
     "safety_checker": pipe.safety_checker,
@@ -35,6 +37,6 @@ super_res_2_pipe.enable_model_cpu_offload()
 
 image = super_res_2_pipe(
     prompt=prompt,
-    image=image,
+    image=pil_image,
 ).images
 image[0].save("./demo/if_stage_3.png")
--- src/diffusers/utils/pil_utils.py
+++ src/diffusers/utils/pil_utils.py
@@ -31,6 +31,11 @@ def pt_to_pil(images):
     images = numpy_to_pil(images)
     return images
 
+def pt_to_numpy(images):
+    images = (images / 2 + 0.5).clamp(0, 1)
+    images = images.cpu().permute(0, 2, 3, 1).float().numpy()
+    return images
+
 
 def numpy_to_pil(images):
     """
