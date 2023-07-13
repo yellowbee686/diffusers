09332759b10127f9700ad9bd3b0309dcc26ded07
yellowbee686 <yellowbee686@gmail.com>
2023-07-13 10:26:49 +0800

    debug

--- sd_xl_demo.py
+++ sd_xl_demo.py
@@ -13,8 +13,7 @@ prompt = "With animation style, An east-asian young, handsome swordman, wears hi
 
 images_1 = pipe(prompt=prompt, output_type="latent").images
 pil_images_1 = pt_to_pil(images_1)
-for i, img in enumerate(pil_images_1):
-    img.save(f"./demo/xl_{i}_1.png")
+pil_images_1.save(f"./demo/xl_1.png")
 
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-0.9", torch_dtype=torch.float16, use_safetensors=True, variant="fp16", local_files_only=True)
 pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
@@ -24,6 +23,6 @@ pipe.to("cuda")
 # pipe.enable_xformers_memory_efficient_attention()
 
 images_2 = pipe(prompt=prompt, image=images_1).images
-pil_images_2 = pt_to_pil(images_2)
 for i, img in enumerate(pil_images_2):
-    img.save(f"./demo/xl_{i}_2.png")
+    img = pt_to_pil(img)
+    img.save(f"./demo/xl_2_{i}.png")
